8bab79d68d7d197a2463263d194bbf6bda6a3415
Cirfa <cirfarangod@gmail.com>
2020-07-26 17:32:52 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch first-version-code
     Your branch is up to date with 'origin/first-version-code'.
    
     Changes to be committed:
    	new file:   .gitattributes
    	new file:   .gitignore
    	new file:   Yssmet/Yssmet.sln
    	new file:   Yssmet/Yssmet/App.config
    	new file:   Yssmet/Yssmet/Program.cs
    	new file:   Yssmet/Yssmet/Properties/AssemblyInfo.cs
    	new file:   Yssmet/Yssmet/Yssmet.csproj
    new file:   Yssmet/Yssmet/packages.config

---
+++ Yssmet/Yssmet/Program.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yssmet
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+
+        }
+    }
+}
